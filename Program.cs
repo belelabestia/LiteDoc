941c43ac8a6d3b1e1d056e968d50094b1fbaf22f
Marco Bellezza <bellezza.m@capp.it>
2021-08-07 13:19:20 +0200

    Init progetto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LiteDoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
