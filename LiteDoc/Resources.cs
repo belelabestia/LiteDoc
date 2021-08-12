006648e20bc3cd316f86a7ea803ca2fcbea2b154
Marco Bellezza <bellezza.m@capp.it>
2021-08-12 04:16:45 +0200

    IOC fatto meglio

--- LiteDoc/Resources.cs
+++ LiteDoc/Resources.cs
@@ -3,20 +3,21 @@ using System.Collections.Generic;
 
 public static class Resources
 {
-    private static Dictionary<Type, Type> instances;
+    private static Dictionary<Type, Type> constructors;
+    private static Dictionary<Type, object> cachedInstances = new Dictionary<Type, object>();
 
     static Resources()
     {
-        instances = new Dictionary<Type, Type>();
-        instances.Add(typeof(IConfiguration), typeof(Configuration.Base));
-        instances.Add(typeof(IDocument), typeof(Document.Base));
-        instances.Add(typeof(IFileSystem), typeof(FileSystem.Base));
-        instances.Add(typeof(IJson), typeof(Json.Base));
-        instances.Add(typeof(ISections), typeof(Sections.Base));
-        instances.Add(typeof(IWatcher), typeof(Watcher.Base));
+        constructors = new Dictionary<Type, Type>();
+        constructors.Add(typeof(IConfiguration), typeof(Configuration.Base));
+        constructors.Add(typeof(IDocument), typeof(Document.Base));
+        constructors.Add(typeof(IFileSystem), typeof(FileSystem.Base));
+        constructors.Add(typeof(IJson), typeof(Json.Base));
+        constructors.Add(typeof(ISections), typeof(Sections.Base));
+        constructors.Add(typeof(IWatcher), typeof(Watcher.Base));
     }
 
-    public static T? Get<T>() => instances.GetValueOrDefault(typeof(T))!.GetInstance<T>();
-    public static void Use<TInterface, TClass>() where TClass : TInterface, new() => instances[typeof(TInterface)] = typeof(TClass);
+    public static T? Get<T>() => (T)cachedInstances.GetValueOrDefault(typeof(T))! ?? constructors.GetValueOrDefault(typeof(T))!.GetInstance<T>();
+    public static void Use<TInterface, TClass>() where TClass : TInterface, new() => constructors[typeof(TInterface)] = typeof(TClass);
     private static T GetInstance<T>(this Type type) => (T)Activator.CreateInstance(type)!;
 }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
-﻿Resources.Use<IConfiguration, VerboseConfiguration>();
-Resources.Use<IDocument, VerboseDocument>();
-Resources.Use<IFileSystem, VerboseFileSystem>();
-Resources.Use<IJson, VerboseJson>();
-Resources.Use<ISections, VerboseSections>();
-Resources.Use<IWatcher, VerboseWatcher>();
+﻿// Resources.Use<IConfiguration, VerboseConfiguration>();
+// Resources.Use<IDocument, VerboseDocument>();
+// Resources.Use<IFileSystem, VerboseFileSystem>();
+// Resources.Use<IJson, VerboseJson>();
+// Resources.Use<ISections, VerboseSections>();
+// Resources.Use<IWatcher, VerboseWatcher>();
+
+Resources.Use<IConfiguration, Configuration.Base>();
+Resources.Use<IDocument, Document.Base>();
+Resources.Use<IFileSystem, FileSystem.Base>();
+Resources.Use<IJson, Json.Base>();
+Resources.Use<ISections, Sections.Base>();
+Resources.Use<IWatcher, Watcher.Base>();
 
 // await args[0].ToLiteDoc().Run();
 await args[0].ToLiteDoc().StartWatching();
